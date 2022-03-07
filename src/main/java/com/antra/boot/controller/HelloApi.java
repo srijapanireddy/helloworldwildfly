37b85f0d4652ce3cff5932ea5bf36991e495e43d
nikitha <nikithareddy167@gmail.com>
2022-03-07 14:54:45 +0530

    1st time file uploaded

---
+++ src/main/java/com/antra/boot/controller/HelloApi.java
@@ -0,0 +1,16 @@
+package com.antra.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/HelloApi")
+public class HelloApi {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+
+}
